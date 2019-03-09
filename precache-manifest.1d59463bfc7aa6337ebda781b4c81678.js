8356329c99acbb1e7cf3d93e5049af214221327d
Vitalik-D <vitalikdyaduchko@gmail.com>
2019-03-09 18:11:39 +0200

    Updates

---
+++ precache-manifest.1d59463bfc7aa6337ebda781b4c81678.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Stars_Wars/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e79e7e17f7824d267b17",
+    "url": "/Stars_Wars/static/js/runtime~main.e3c523a6.js"
+  },
+  {
+    "revision": "92fc52688d3e7be3709e",
+    "url": "/Stars_Wars/static/js/main.ceb1e368.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/Stars_Wars/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "92fc52688d3e7be3709e",
+    "url": "/Stars_Wars/static/css/main.41933ebf.chunk.css"
+  },
+  {
+    "revision": "9417eaf9a9c4545390f5b917658574a4",
+    "url": "/Stars_Wars/index.html"
+  }
+];
